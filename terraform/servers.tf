fa908e565a714d94f962959fd01dd92e3fad7d3e
anusha <anushag2k16@gmail.com>
2023-04-24 21:02:13 +0530

    committing to roboshop-shell

--- terraform/servers.tf
+++ terraform/servers.tf
@@ -1,8 +1,8 @@
-data "aws_ami" "centos"
-{
+data "aws_ami" "centos"{
+  owners           = [" 973714476881"]
+
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
-  owners           = [" 973714476881"]
 }
 resource "aws_instance" "frontend" {
   ami           = "data.aws_ami.image_id"
