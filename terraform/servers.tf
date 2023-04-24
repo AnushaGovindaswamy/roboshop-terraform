870e704c79c3d21d1439d7c81251831279b0cc26
anusha <anushag2k16@gmail.com>
2023-04-24 22:03:28 +0530

    committing to roboshop-shell

--- terraform/servers.tf
+++ terraform/servers.tf
@@ -18,6 +18,13 @@ resource "aws_instance" "frontend" {
   output "frontend" {
   value = aws_instance.frontend.public_ip
   }
+  resource "aws_route53_record" "frontend" {
+    zone_id = "Z0941133DH3UYAXI04QH"
+    name    = "frontend-dev.anushadevopsb72.online"
+    type    = "A"
+    ttl     = 30
+    records = [aws_instance.frontend.private_ip]
+  }
   resource "aws_instance" "mongodb" {
     ami           = data.aws_ami.centos.image_id
     instance_type = "t3.micro"
@@ -26,6 +33,13 @@ resource "aws_instance" "frontend" {
       Name = "mongodb"
     }
     }
+    resource "aws_route53_record" "mongodb" {
+        zone_id = "Z0941133DH3UYAXI04QH"
+        name    = "mongodb-dev.anushadevopsb72.online"
+        type    = "A"
+        ttl     = 30
+        records = [aws_instance.mongodb.private_ip]
+      }
   resource "aws_instance" "catalogue" {
      ami           = data.aws_ami.centos.image_id
      instance_type = "t3.micro"
@@ -34,6 +48,13 @@ resource "aws_instance" "frontend" {
        Name = "catalogue"
      }
      }
+     resource "aws_route53_record" "catalogue" {
+             zone_id = "Z0941133DH3UYAXI04QH"
+             name    = "catalogue-dev.anushadevopsb72.online"
+             type    = "A"
+             ttl     = 30
+             records = [aws_instance.catalogue.private_ip]
+           }
    resource "aws_instance" "redis" {
       ami           = data.aws_ami.centos.image_id
       instance_type = "t3.micro"
@@ -42,6 +63,13 @@ resource "aws_instance" "frontend" {
         Name = "redis"
       }
       }
+      resource "aws_route53_record" "redis" {
+                   zone_id = "Z0941133DH3UYAXI04QH"
+                   name    = "redis-dev.anushadevopsb72.online"
+                   type    = "A"
+                   ttl     = 30
+                   records = [aws_instance.redis.private_ip]
+                 }
     resource "aws_instance" "user" {
        ami           = data.aws_ami.centos.image_id
        instance_type = "t3.micro"
@@ -50,6 +78,13 @@ resource "aws_instance" "frontend" {
          Name = "user"
        }
        }
+       resource "aws_route53_record" "user" {
+                          zone_id = "Z0941133DH3UYAXI04QH"
+                          name    = "user-dev.anushadevopsb72.online"
+                          type    = "A"
+                          ttl     = 30
+                          records = [aws_instance.user.private_ip]
+                        }
    resource "aws_instance" "cart" {
       ami           = data.aws_ami.centos.image_id
       instance_type = "t3.micro"
@@ -58,6 +93,13 @@ resource "aws_instance" "frontend" {
         Name = "cart"
       }
       }
+      resource "aws_route53_record" "cart" {
+                                zone_id = "Z0941133DH3UYAXI04QH"
+                                name    = "cart-dev.anushadevopsb72.online"
+                                type    = "A"
+                                ttl     = 30
+                                records = [aws_instance.cart.private_ip]
+                              }
    resource "aws_instance" "mysql" {
       ami           = data.aws_ami.centos.image_id
       instance_type = "t3.micro"
@@ -66,6 +108,13 @@ resource "aws_instance" "frontend" {
         Name = "mysql"
       }
       }
+       resource "aws_route53_record" "mysql" {
+                                zone_id = "Z0941133DH3UYAXI04QH"
+                                name    = "mysql-dev.anushadevopsb72.online"
+                                type    = "A"
+                                ttl     = 30
+                                records = [aws_instance.mysql.private_ip]
+                              }
     resource "aws_instance" "shipping" {
        ami           = data.aws_ami.centos.image_id
        instance_type = "t3.micro"
@@ -74,6 +123,13 @@ resource "aws_instance" "frontend" {
          Name = "shipping"
        }
        }
+       resource "aws_route53_record" "shipping" {
+                                       zone_id = "Z0941133DH3UYAXI04QH"
+                                       name    = "shipping-dev.anushadevopsb72.online"
+                                       type    = "A"
+                                       ttl     = 30
+                                       records = [aws_instance.shipping.private_ip]
+                                     }
        resource "aws_instance" "rabbitmq" {
              ami           = data.aws_ami.centos.image_id
              instance_type = "t3.micro"
@@ -82,6 +138,13 @@ resource "aws_instance" "frontend" {
                Name = "rabbitmq"
              }
              }
+              resource "aws_route53_record" "rabbitmq" {
+                     zone_id = "Z0941133DH3UYAXI04QH"
+                     name    = "rabbitmq-dev.anushadevopsb72.online"
+                     type    = "A"
+                     ttl     = 30
+                     records = [aws_instance.rabbitmq.private_ip]
+                   }
            resource "aws_instance" "payment" {
               ami           = data.aws_ami.centos.image_id
               instance_type = "t3.micro"
@@ -90,6 +153,13 @@ resource "aws_instance" "frontend" {
                 Name = "payment"
               }
               }
+               resource "aws_route53_record" "payment" {
+                      zone_id = "Z0941133DH3UYAXI04QH"
+                      name    = "payment-dev.anushadevopsb72.online"
+                      type    = "A"
+                      ttl     = 30
+                      records = [aws_instance.payment.private_ip]
+                    }
               resource "aws_instance" "dispatch" {
                             ami           = data.aws_ami.centos.image_id
                             instance_type = "t3.micro"
@@ -98,4 +168,11 @@ resource "aws_instance" "frontend" {
                               Name = "dispatch"
                             }
                             }
+                             resource "aws_route53_record" "dispatch" {
+                                    zone_id = "Z0941133DH3UYAXI04QH"
+                                    name    = "dispatch-dev.anushadevopsb72.online"
+                                    type    = "A"
+                                    ttl     = 30
+                                    records = [aws_instance.dispatch.private_ip]
+                                  }
 
