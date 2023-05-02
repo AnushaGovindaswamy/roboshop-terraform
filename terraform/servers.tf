ecfca247eaf60f2cf03a64f06e6406dd9440e815
anusha <anushag2k16@gmail.com>
2023-05-02 21:34:02 +0530

    committing to roboshop-shell

--- terraform/servers.tf
+++ terraform/servers.tf
@@ -69,8 +69,8 @@ name = "frontend"
 
 }
 resource "aws_instance" "instance" {
-for-each =var.components
-  ami           = data.aws_ami.centos.image_id
+  for-each =var.components
+ami           = data.aws_ami.centos.image_id
   instance_type = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.selected.id]
 
