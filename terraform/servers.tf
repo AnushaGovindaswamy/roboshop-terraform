ce285c966fec4474137ba44958f71f8992b607a4
anusha <anushag2k16@gmail.com>
2023-05-02 21:40:18 +0530

    committing to roboshop-shell

--- terraform/servers.tf
+++ terraform/servers.tf
@@ -17,7 +17,7 @@ output "aws_ami" {
 value=data.aws_ami.centos.image_id
 }
 variable "components2" {
-default = ["frontend","mongodb","catalogue","redis","cart","user","shipping","payment","mysql","rabbitmq","dispatch"]
+default = ["frontend1","mongodb1","dispatch1"]
 }
 variable "components" {
 default = {
