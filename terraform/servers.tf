8501a9312964681ca078079c01d466744c539fd4
anusha <anushag2k16@gmail.com>
2023-05-02 21:41:57 +0530

    committing to roboshop-shell

--- terraform/servers.tf
+++ terraform/servers.tf
@@ -100,12 +100,5 @@ resource "aws_route53_record" "records" {
       Name = var.components2[count.index]
     }
     }
-    resource "aws_route53_record" "records2" {
-     count =length(var.components2)
-        zone_id = "Z0941133DH3UYAXI04QH"
-        name    = " ${var.components2[count.index]}-dev.anushadevopsb72.online"
-        type    = "A"
-        ttl     = 30
-        records = [aws_instance.var.components2[count.index].private_ip]
-      }
+
 
