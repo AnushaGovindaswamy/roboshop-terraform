be870d8fb1033a3dcdca860279f376b47199f055
anusha <anushag2k16@gmail.com>
2023-05-07 07:22:48 +0530

    committing to roboshop-terraform

--- servers.tf
+++ servers.tf
@@ -14,18 +14,17 @@ resource "null_resource" "provisioner" {
 depends_on = [aws_instance.instance,aws_route53_record.records]
 for_each=var.components
 
+provisioner "remote-exec" {
   connection {
-    type     = "ssh"
-          user     = "centos"
-          password = "DevOps321"
-          host     = aws_instance.instance[each.value["name"]].private_ip
-  }
-
-  provisioner "remote-exec" {
+      type     = "ssh"
+            user     = "centos"
+            password = "DevOps321"
+            host     = aws_instance.instance[each.value["name"]].private_ip
+    }
   inline = [
       "rm -rf roboshop-shell",
-      "https://github.com/AnushaGovindaswamy/roboshop-shell.git",
-      "cd roboshop-shell",
+      "https://github.com/AnushaGovindaswamy/roboshop-shell.git,
+      "cd roboshop-shell,
       "sudo bash ${each.value["name"]}.sh ${lookup(each.value,"password","null")}",
 
       ]
